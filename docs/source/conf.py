3676b32c5851332f5de9832eda5c04179d2c6197
hbenedek <hben.0204@gmail.com>
2023-04-17 11:24:07 +0200

    config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,11 +24,6 @@ extensions = [
     'sphinx_toolbox.more_autodoc',
     'sphinx.ext.autodoc']
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
