1811598c074262cd3fdbf630b9c62bedbe1a6e45
hbenedek <hben.0204@gmail.com>
2023-04-17 14:29:08 +0200

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,3 +27,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
